32430b40a6d2c1bd65b2db82f130f4ed6ebe7d05
Swann Perarnau <swann@anl.gov>
2022-04-19 15:16:42 -0500

    fix nix, variorum not a pkgconfig setup

--- nix/nrm-extra.nix
+++ nix/nrm-extra.nix
@@ -1,7 +1,7 @@
-{ stdenv, autoreconfHook, pkgconfig, libnrm, gfortran, mpich2, openmp, papi, jansson, variorum }:
+{ stdenv, autoreconfHook, pkgconfig, libnrm, gfortran, mpich2, openmp, papi, jansson, variorum, hwloc }:
 stdenv.mkDerivation {
   src = ../.;
   name = "nrm-extra";
-  nativeBuildInputs = [ autoreconfHook pkgconfig libnrm mpich2 openmp papi jansson variorum ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig libnrm mpich2 openmp papi jansson variorum hwloc ];
   buildInputs = [ gfortran ] ++ libnrm.buildInputs;
 }
