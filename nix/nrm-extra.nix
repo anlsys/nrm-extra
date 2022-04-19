7595fc41bac2ccfff60f71502514955a2d33d23a
Swann Perarnau <swann@anl.gov>
2022-04-19 13:44:08 -0500

    [fix] fix builds

--- nix/nrm-extra.nix
+++ nix/nrm-extra.nix
@@ -1,7 +1,7 @@
-{ stdenv, autoreconfHook, pkgconfig, libnrm, gfortran, mpich2, openmp, papi }:
+{ stdenv, autoreconfHook, pkgconfig, libnrm, gfortran, mpich2, openmp, papi, jansson, variorum }:
 stdenv.mkDerivation {
   src = ../.;
   name = "nrm-extra";
-  nativeBuildInputs = [ autoreconfHook pkgconfig libnrm mpich2 openmp papi jansson ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig libnrm mpich2 openmp papi jansson variorum ];
   buildInputs = [ gfortran ] ++ libnrm.buildInputs;
 }
