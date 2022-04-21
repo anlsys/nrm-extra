c7d77ebadac389f8fe99c721a91a4fc64b170f29
jlnav <jnavarro@anl.gov>
2022-04-21 10:19:17 -0500

    specify configureFlags to VARIORUM_ vars

--- nix/nrm-extra.nix
+++ nix/nrm-extra.nix
@@ -2,6 +2,7 @@
 stdenv.mkDerivation {
   src = ../.;
   name = "nrm-extra";
+  configureFlags = [ "VARIORUM_CFLAGS="-I${variorum}/include"" "VARIORUM_LIBS="-L${variorum}/lib -lvariorum"" ];
   nativeBuildInputs = [ autoreconfHook pkgconfig libnrm mpich2 openmp papi jansson variorum hwloc ];
   buildInputs = [ gfortran ] ++ libnrm.buildInputs;
 }
