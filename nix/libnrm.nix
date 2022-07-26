6841c100a6e2a6a2d702f5b8c092834c4fd69a4a
jlnav <jnavarro@anl.gov>
2022-07-26 10:13:28 -0500

    update libnrm.nix for new dependencies

--- nix/libnrm.nix
+++ nix/libnrm.nix
@@ -1,10 +1,10 @@
-{ stdenv, autoreconfHook, pkgconfig, zeromq, czmq, gfortran }:
+{ stdenv, autoreconfHook, pkgconfig, zeromq, czmq, gfortran, jansson, check, protobufc, git }:
 stdenv.mkDerivation {
   src = fetchGit {
     url = "https://github.com/anlsys/libnrm.git";
     ref = "master";
   };
   name = "libnrm";
-  nativeBuildInputs = [ autoreconfHook pkgconfig ];
-  buildInputs = [ zeromq gfortran ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig git ];
+  buildInputs = [ zeromq gfortran czmq jansson check protobufc ];
 }
