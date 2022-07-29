05524a6f8f41dbd3a3621b0f58c4bf4fecd5258c
jlnav <jnavarro@anl.gov>
2022-07-29 10:30:21 -0500

    rearrange nrm_omp.h, rename time var

--- src/omp/nrm_omp.h
+++ src/omp/nrm_omp.h
@@ -22,11 +22,24 @@ extern nrm_client_t *global_client;
 extern nrm_scope_t *global_scope;
 extern nrm_sensor_t *global_sensor;
 
-extern char *upstream_uri = "tcp://127.0.0.1";
-extern int pub_port = 2345;
-extern int rpc_port = 3456;
+extern char *upstream_uri;
+extern int pub_port;
+extern int rpc_port;
 
 extern ompt_set_callback_t nrm_ompt_set_callback;
+
+extern nrm_time_t nrmtime;
+
+upstream_uri = "tcp://127.0.0.1";
+pub_port = 2345;
+rpc_port = 3456;
+
+nrm_client_create(&global_client, upstream_uri, pub_port, rpc_port);
+
+global_scope = nrm_scope_create();
+
+char *name = "nrm-omp";
+global_sensor = nrm_sensor_create(name);
 
 void nrm_ompt_register_cbs(void);
 
--- src/omp/nrm_omp_callbacks.c
+++ src/omp/nrm_omp_callbacks.c
@@ -3,7 +3,6 @@
 
 #include "nrm_omp.h"
 
-nrm_time_t time;
 
 void nrm_ompt_callback_thread_begin_cb(ompt_thread_t thread_type,
                                        ompt_data_t *thread_data)
@@ -12,10 +11,11 @@ void nrm_ompt_callback_thread_begin_cb(ompt_thread_t thread_type,
 	thread_data->ptr = (void *)scope;
 
 	nrm_scope_threadprivate(scope);
+
 	nrm_scope_threadshared(global_scope);
 
-	nrm_time_gettime(&time);
-	nrm_client_send_event(global_client, time, global_sensor, scope, 1);
+	nrm_time_gettime(&nrmtime);
+	nrm_client_send_event(global_client, nrmtime, global_sensor, scope, 1);
 }
 
 void nrm_ompt_callback_thread_end_cb(ompt_data_t *thread_data)
@@ -23,9 +23,9 @@ void nrm_ompt_callback_thread_end_cb(ompt_data_t *thread_data)
 	nrm_scope_t *scope = (nrm_scope_t *)thread_data->ptr;
 	nrm_scope_threadprivate(scope);
 
-	nrm_time_gettime(&time);
-	nrm_client_send_event(global_client, time, global_sensor, scope, 1);
-	nrm_scope_destroy(scope);
+	nrm_time_gettime(&nrmtime);
+	nrm_client_send_event(global_client, nrmtime, global_sensor, scope, 1);
+	nrm_scope_destroy(&scope);
 	thread_data->ptr = NULL;
 }
 
@@ -37,8 +37,8 @@ void nrm_ompt_callback_parallel_begin_cb(
         int flags,
         const void *codeptr_ra)
 {
-	nrm_time_gettime(&time);
-	nrm_client_send_event(global_client, time, global_sensor, global_scope, 1);
+	nrm_time_gettime(&nrmtime);
+	nrm_client_send_event(global_client, nrmtime, global_sensor, global_scope, 1);
 }
 
 void nrm_ompt_callback_parallel_end_cb(ompt_data_t *parallel_data,
@@ -46,8 +46,8 @@ void nrm_ompt_callback_parallel_end_cb(ompt_data_t *parallel_data,
                                        int flags,
                                        const void *codeptr_ra)
 {
-	nrm_time_gettime(&time);
-	nrm_client_send_event(global_client, time, global_sensor, global_scope, 1);
+	nrm_time_gettime(&nrmtime);
+	nrm_client_send_event(global_client, nrmtime, global_sensor, global_scope, 1);
 }
 
 void nrm_ompt_callback_work_cb(ompt_work_t wstype,
