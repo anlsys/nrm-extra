30f93fd838df25c58810fadd759b3c6208e77242
jlnav <jnavarro@anl.gov>
2022-08-04 11:40:12 -0500

    add git as native build input for nrm-extra.nix

--- nix/nrm-extra.nix
+++ nix/nrm-extra.nix
@@ -1,8 +1,8 @@
-{ stdenv, autoreconfHook, pkgconfig, libnrm, gfortran, mpich2, openmp, papi, jansson, variorum, hwloc }:
+{ stdenv, autoreconfHook, pkgconfig, libnrm, gfortran, mpich2, openmp, papi, jansson, variorum, hwloc git }:
 stdenv.mkDerivation {
   src = ../.;
   name = "nrm-extra";
-  nativeBuildInputs = [ autoreconfHook pkgconfig libnrm mpich2 openmp papi jansson variorum hwloc ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig libnrm mpich2 openmp papi jansson variorum hwloc git ];
   buildInputs = [ gfortran ] ++ libnrm.buildInputs;
   VARIORUM_CFLAGS = "-I${variorum}/include";
   VARIORUM_LIBS = "-L${variorum}/lib -lvariorum";
