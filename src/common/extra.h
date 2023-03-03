fb238e325ab3c854dd3bf7bac59d01c1b5c87c29
Swann Perarnau <swann@anl.gov>
2023-03-03 10:16:59 -0600

    fixes

--- src/common/extra.h
+++ src/common/extra.h
@@ -2,8 +2,8 @@
  * Copyright 2021 UChicago Argonne, LLC.
  * (c.f. AUTHORS, LICENSE)
  *
- * This file is part of the libnrm project.
- * For more info, see https://github.com/anlsys/libnrm
+ * This file is part of the nrm-extra project.
+ * For more info, see https://github.com/anlsys/nrm-extra
  *
  * SPDX-License-Identifier: BSD-3-Clause
  *******************************************************************************/
