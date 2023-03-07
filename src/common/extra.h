f2ec9e6f9c9984da66fd98ae4de0e8bc447cef9f
jlnav <jnavarro@anl.gov>
2023-03-07 09:42:14 -0600

    clang-format

--- src/common/extra.h
+++ src/common/extra.h
@@ -12,7 +12,10 @@
 #define NRM_EXTRA_H 1
 
 int nrm_extra_create_name(const char *pattern, char **name);
-int nrm_extra_create_name_ssu(const char *pattern, const char *extra, unsigned int idx, char **name);
+int nrm_extra_create_name_ssu(const char *pattern,
+                              const char *extra,
+                              unsigned int idx,
+                              char **name);
 int nrm_extra_find_allowed_scope(nrm_client_t *client,
                                  const char *toolname,
                                  nrm_scope_t **scope,
