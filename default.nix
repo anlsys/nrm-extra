74d44883a526eb59e01390fa2ea065824a32ed8a
Swann Perarnau <swann@anl.gov>
2023-04-20 11:29:54 -0500

    [fix] can't move PMPI Makefile.am
    Removing PMPI Makefile.am was a mistake, it forces mpicc on all
    products.

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/22.05.tar.gz") {},
 }:
 pkgs // rec {
-  stdenv = pkgs.stdenvAdapters.keepDebugInfo pkgs.llvmPackages_14.stdenv;
+  stdenv = pkgs.stdenvAdapters.keepDebugInfo pkgs.llvmPackages_12.stdenv;
   libnrm = pkgs.callPackage ./nix/libnrm.nix { inherit stdenv; };
   variorum = pkgs.callPackage ./nix/variorum.nix { };
   nrm-extra = pkgs.callPackage ./nix/nrm-extra.nix { inherit libnrm; inherit variorum; openmp = pkgs.llvmPackages_12.openmp; };
