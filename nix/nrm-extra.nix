26c0fc44099e47068fb066fff03c4748af934657
jlnav <jnavarro@anl.gov>
2023-06-28 11:21:50 -0500

    add geopm cflags, libs, configure option, etc. to nrm-extra.nix

--- nix/nrm-extra.nix
+++ nix/nrm-extra.nix
@@ -4,6 +4,10 @@ stdenv.mkDerivation {
   name = "nrm-extra";
   nativeBuildInputs = [ autoreconfHook pkgconfig git ];
   buildInputs = [ gfortran libnrm mpich openmp papi jansson variorum hwloc geopmd ];
+  configureFlags = "--with-geopm --with-variorum";
   VARIORUM_CFLAGS = "-I${variorum}/include";
   VARIORUM_LIBS = "-L${variorum}/lib -lvariorum";
+  GEOPM_CFLAGS = "-I${geopmd}/include";
+  GEOPM_LIBS = "-L${geopmd}/lib -lgeopmd";
+  CFLAGS = "-Wall -Wextra";
 }
