9b29cd3e9dda179445946d6f331f1d83cd4b2961
Kamil Iskra <iskra@mcs.anl.gov>
2023-08-28 15:43:07 -0500

    Undo changes to default.nix

--- default.nix
+++ default.nix
@@ -4,5 +4,6 @@ pkgs // rec {
   stdenv = pkgs.stdenvAdapters.keepDebugInfo pkgs.llvmPackages_12.stdenv;
   libnrm = pkgs.callPackage ./nix/libnrm.nix { inherit stdenv; };
   variorum = pkgs.callPackage ./nix/variorum.nix { };
-  nrm-extra = pkgs.callPackage ./nix/nrm-extra.nix { inherit libnrm; inherit variorum; };
+  geopmd = pkgs.callPackage ./nix/geopmd.nix { openmp = pkgs.llvmPackages_12.openmp; };
+  nrm-extra = pkgs.callPackage ./nix/nrm-extra.nix { inherit libnrm; inherit variorum; openmp = pkgs.llvmPackages_12.openmp; inherit geopmd;};
 }
