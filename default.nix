7a60a5fcf136b8244c61cf1345ab60b5686194be
Kamil Iskra <iskra@mcs.anl.gov>
2023-08-28 16:02:01 -0500

    Let's try this again...

--- default.nix
+++ default.nix
@@ -2,8 +2,8 @@
 }:
 pkgs // rec {
   stdenv = pkgs.stdenvAdapters.keepDebugInfo pkgs.llvmPackages_12.stdenv;
-  libnrm = pkgs.callPackage ./nix/libnrm.nix { inherit stdenv; };
   variorum = pkgs.callPackage ./nix/variorum.nix { };
   geopmd = pkgs.callPackage ./nix/geopmd.nix { openmp = pkgs.llvmPackages_12.openmp; };
-  nrm-extra = pkgs.callPackage ./nix/nrm-extra.nix { inherit libnrm; inherit variorum; openmp = pkgs.llvmPackages_12.openmp; inherit geopmd;};
+  libnrm = pkgs.callPackage ./nix/libnrm.nix { openmp = pkgs.llvmPackages_15.openmp; inherit geopmd; };
+  nrm-extra = pkgs.callPackage ./nix/nrm-extra.nix { inherit libnrm; inherit variorum;};
 }
